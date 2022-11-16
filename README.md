ee3d94539c0f44e674805ba2be6ad57d6f0091b1
BeatrizCastro274 <111613357+BeatrizCastro274@users.noreply.github.com>
2022-11-15 21:45:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular-att-07
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-zp65bi)
---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, VERSION } from "@angular/core";
+import { BitcoinService } from "./bitcoin.service";
+import { TimerService } from "./timer.service";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  constructor(
+    public BitcoinService: BitcoinService,
+    public timer: TimerService
+  ) {
+    this.timer.start();
+  }
+
+  ngOnInit() {}
+}
